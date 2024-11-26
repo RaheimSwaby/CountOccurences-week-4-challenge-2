5591063e08d7c90915c773a69680790e7a4ec966
Rahei <Rahei@RAHEIMSPC>
2024-11-26 14:44:04 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CountOccurences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> Count = new Dictionary<string,int>();
+            string[] input = [ "a", "b", "c","a","c", "d","h" ,"f"];
+
+            foreach (string s in input)
+            {
+                if (!Count.TryAdd(s,1)) 
+                {
+                    Count[s]++;
+                }
+            }
+            Console.WriteLine();
+            foreach (KeyValuePair<string, int> kvp in Count)
+            {
+                Console.WriteLine("key = {0}, count = {1}", kvp.Key,kvp.Value);
+            }  
+
+        }
+    }
+}
